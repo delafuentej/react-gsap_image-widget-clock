dccb2913d26961e895700ed409f43eb910a6e4d2
Jesús de la Fuente <jfuentelucas@gmail.com>
2026-01-13 11:55:14 +0100

    chore: prepare build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  chunkSizeWarningLimit: 2000,
+  rollupOptions: {
+    output: {
+      manualChunks(id) {
+        if (id.includes("node_modules/react")) return "react";
+      },
+    },
+  },
+  plugins: [visualizer({ open: true }), react()],
+});
